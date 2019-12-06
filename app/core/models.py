046b1134aa9f681e92e616f9b4a29d1bbaa01f0b
Thiaka Badji <izzybadji@gmail.com>
2019-12-06 13:05:10 +0000

    user email validation

--- app/core/models.py
+++ app/core/models.py
@@ -3,9 +3,12 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 
 # Create your models here.
 class UserManager(BaseUserManager):
+    
     def create_user(self, email, password=None, **extra_fields):
-        """Creates and saves a new user """
-        user = self.model(email=email, **extra_fields)
+        """ Creates and saves a new user """
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
@@ -13,7 +16,7 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """Custom user model that using email instead of username"""
+    """ Custom user model that using email instead of username """
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
